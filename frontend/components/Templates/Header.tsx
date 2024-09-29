4781d01165904503af846e7faabc60834afad8ed
yusuken1121 <yusuken1121@gmail.com>
2024-09-28 20:23:41 -0700

    💄 Add an animation

--- frontend/components/Templates/Header.tsx
+++ frontend/components/Templates/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import * as motion from "framer-motion/client";
 const Header = () => {
   return (
     <header className="fixed top-0 left-0 flex flex-col items-between justify-center w-full h-[8lvh] bg-white bg-opacity-80 text-black z-50">
@@ -7,26 +7,42 @@ const Header = () => {
         <div>logo</div>
         <nav role="navigation">
           <ul className="flex items-center justify-between gap-6">
-            <li>
+            <motion.li
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, ease: "easeIn" }}
+            >
               <a href="#about" id="about" className="hover:text-gray-600">
                 About
               </a>
-            </li>
-            <li>
+            </motion.li>
+            <motion.li
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, ease: "easeIn", delay: 0.2 }}
+            >
               <a href="#skills" id="skills" className="hover:text-gray-600">
                 Skills
               </a>
-            </li>
-            <li>
+            </motion.li>
+            <motion.li
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, ease: "easeIn", delay: 0.4 }}
+            >
               <a href="#works" id="works" className="hover:text-gray-600">
                 Works
               </a>
-            </li>
-            <li>
+            </motion.li>
+            <motion.li
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, ease: "easeIn", delay: 0.6 }}
+            >
               <a href="#contact" id="contact" className="hover:text-gray-600">
                 Contact
               </a>
-            </li>
+            </motion.li>
           </ul>
         </nav>
       </div>
